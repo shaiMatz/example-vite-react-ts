8310006e56123a23ded45b1a1b07c8a18846ad01
shaiMatz <shyshir@gmail.com>
2023-11-17 12:01:29 +0200

    first transfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,61 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import wharfLogo from './assets/wharf.svg'
-import './App.css'
-import { Chains, Session, SessionKit } from '@wharfkit/session'
-import { WalletPluginAnchor } from '@wharfkit/wallet-plugin-anchor'
-import WebRenderer from '@wharfkit/web-renderer'
-import React from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import "./App.css";
+import WebRenderer from "@wharfkit/web-renderer";
+import React from "react";
+import metamask from "@metamask/onboarding";
+import { useSDK } from "@metamask/sdk-react";
+import { Session } from "@wharfkit/session";
+import { WalletPluginPrivateKey } from "@wharfkit/wallet-plugin-privatekey";
+import { Result } from "ethers";
 
-const sessionKit = new SessionKit({
-  appName: 'demo',
-  chains: [Chains.Jungle4],
-  ui: new WebRenderer(),
-  walletPlugins: [
-      new WalletPluginAnchor(),
-  ],
-})
+const accountName = "wharfkit1111";
+const permissionName = "test";
 
-function App() {
-  const [session, setSession]: [Session | undefined, Dispatch<SetStateAction<Session | undefined>>] = useState()
-  
-  useEffect(() => {
-    sessionKit.restore().then((restored) => setSession(restored))
-  }, [])
-  
-  async function login() {
-      const response = await sessionKit.login()
-      setSession(response.session)
-  }
+const chain = {
+  id: "73e4385a2708e6d7048834fbc1079f2fabb17b3c125b146af438971e90716c4d",
+  url: "https://jungle4.greymass.com",
+};
+const privateKey = "5Jtoxgny5tT7NiNFp1MLogviuPJ9NniWjnU4wKzaX4t7pL4kJ8s";
+const walletPlugin = new WalletPluginPrivateKey(privateKey);
 
-  async function logout() {
-      sessionKit.logout(session)
-      setSession(undefined)
-  }
+const session = new Session({
+  actor: accountName,
+  permission: permissionName,
+  chain,
+  walletPlugin,
+});
 
-  async function transact() {
-      if (!session) {
-          throw new Error('Cannot transact without a session.')
-      }
-      const action = {
-          account: 'eosio.token',
-          name: 'transfer',
-          authorization: [session.permissionLevel],
-          data: {
-              from: session.actor,
-              to: 'teamgreymass',
-              quantity: '0.0001 EOS',
-              memo: 'Yay WharfKit! Thank you <3',
-          },
-      }
-      session.transact({ action }, { broadcast: false }).catch((e) => {
-          console.log('error caught in transact', e)
-      })
-  }
+const transferAction = {
+  account: "eosio.token",
+  name: "transfer",
+  authorization: [session.permissionLevel],
+  data: {
+    from: session.actor,
+    to: "wharfkittest",
+    quantity: "0.0001 EOS",
+    memo: "Hello World! from Shai",
+  },
+};
+
+const App = () => {
+  let temp;
+
+  const onClick = async () => {
+    const result = await session.transact({ action: transferAction });
+    temp = result;
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://wharfkit.com" target="_blank">
-          <img src={wharfLogo} className="logo wharf" alt="Wharf logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Wharf</h1>
-      <div className="card">
-        {!session ? (
-          <button className="primary" onClick={login}> Login </button>
-        ): (
-          <React.Fragment>
-            <p>{String(session.actor)}</p>
-            <button className="primary" onClick={transact}>
-                Test Transaction (No Broadcast)
-            </button>
-            <button onClick={logout}> Logout </button>
-          </React.Fragment>
-        )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite, React, and Wharf logos to learn more
-      </p>
-    </div>
-  )
-}
+      <header className="App-header">
+        <h1>WharfKit React Demo 123</h1>
 
-export default App
+        <button onClick={onClick}>Transfer</button>
+
+        {temp}
+      </header>
+    </div>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        checkInstallationImmediately: false,
+        dappMetadata: {
+          name: "Demo React App",
+          url: window.location.host,
+        },
+      }}
+    >
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
-)
+);
